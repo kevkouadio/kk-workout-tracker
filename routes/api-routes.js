8ac06441f25bef6f1d2b8424300ce8e71701d1ac
Kouassi Kevin Kouadio <kevkouadio@gmil.com>
2020-10-23 12:55:23 -0400

    add of post new exercise api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,21 +1,25 @@
 const db = require("../models");
 
 module.exports = function(app) {
-
+//display last workout on home page
   app.get("/api/workouts", function(req, res) {
     db.Workout.find({}).then(function(dbWorkouts) {
       res.json(dbWorkouts);
     });
   });
-
-  
-
+//add a new workout exercise
+  app.post("/api/workouts", function({ body }, res) {
+    db.Workout.create(body).then(function(dbWorkouts) {
+      res.json(dbWorkouts);
+    });
+  });
+//update exercise
   app.put("/api/workouts/:id", function(req, res) {
     db.Workout.findOneAndUpdate({_id:req.params.id}, {$set:  {exercises: req.body}}, { new: true } ).then(function(dbWorkouts) {
       res.json(dbWorkouts);
     });
   });
-
+//display graph of exercises
   app.get("/api/workouts/range", function(req, res) {
     db.Workout.find({}).populate("workouts").then(function(dbWorkouts) {
       res.json(dbWorkouts);
