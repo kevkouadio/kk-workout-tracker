0e2d340eedbd877be8b73393fd936cc915d281b1
Kouassi Kevin Kouadio <kevkouadio@gmil.com>
2020-10-23 12:42:29 -0400

    add of update and populate api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 
 module.exports = function(app) {
+
   app.get("/api/workouts", function(req, res) {
     db.Workout.find({}).then(function(dbWorkouts) {
       res.json(dbWorkouts);
@@ -8,4 +9,16 @@ module.exports = function(app) {
   });
 
   
+
+  app.put("/api/workouts/:id", function(req, res) {
+    db.Workout.findOneAndUpdate({_id:req.params.id}, {$set:  {exercises: req.body}}, { new: true } ).then(function(dbWorkouts) {
+      res.json(dbWorkouts);
+    });
+  });
+
+  app.get("/api/workouts/range", function(req, res) {
+    db.Workout.find({}).populate("workouts").then(function(dbWorkouts) {
+      res.json(dbWorkouts);
+    });
+  });
 };
